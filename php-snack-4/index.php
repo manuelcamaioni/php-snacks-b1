def25894f33512bd69795e953587205bb00da1e0
manuelcamaioni <camaioni.manuel@gmail.com>
2023-07-04 12:52:18 +0200

    snack 4

---
+++ php-snack-4/index.php
@@ -0,0 +1,28 @@
+<!-- Scrivi una funzione che accetti tre argomenti:
+un array e due numeri ("a" più piccolo di "b" e "b" grande al massimo quanto il numero di elementi dell'array).
+
+La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa 
+tra "a" e "b" -->
+
+<?php 
+
+$array = ['a', 'b', 'c', 'd', 'e'];
+function createArray($array1, $a, $b){
+
+    $newArray = [];
+
+    if($a < $b && $b <= count($array1)){
+
+        for($i = 0; $i < $b; $i++){
+            $newArray[] = $array1[$i];
+        }
+        
+    }
+    return $newArray;
+}
+
+$generatedArray = [createArray($array, 1, 3)];
+var_dump($generatedArray);
+
+
+?>
